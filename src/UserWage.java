7e30a063fef0cd0ef74d347ce3807dcbc5e3c139
domHenin <domhenin.517@gmail.com>
2019-04-22 20:48:36 -0400

    reWork complete

---
+++ src/UserWage.java
@@ -0,0 +1,17 @@
+public class UserWage {
+    private double salary=0;
+
+//    public UserWage(double salary) { this.salary = salary; }
+
+    public void setSalary(double salary) { this.salary = salary; }
+
+    public double getSalary() { return salary; }
+
+//    public void UserAge() {
+//        System.out.println("How much do you make?");
+//    }
+}
+/// left off here
+///TODO:
+// call from 'main' all the created methods
+//
